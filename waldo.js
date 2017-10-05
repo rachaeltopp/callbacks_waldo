a6fad063142c2fca79934a16a4c28bb55b1dd8ca
Rachael Topp <topp@ualberta.ca>
2017-10-05 15:11:00 +0000

    used forEach method

--- waldo.js
+++ waldo.js
@@ -1,24 +1,12 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
+  if (arr[i] === "Waldo") {
       found(i);   // execute callback
-    }
   }
 }
 
-function actionWhenFound(i) {
-  console.log("Found Waldo at index " + i + "!");
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + "!");
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+arr.forEach(findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 
-
-
-
-
-/*
-Modify the callback function in the previous example
-so that logs the index of the array where Waldo is found,
-ie. "Found Waldo at index 2!". (You will need to modify
-actionWhenFound to receive the index.)
-*/
